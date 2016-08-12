cce93ddbd936fe3babaa42f21d387fddbeae0f7e
garedean <garrettdolson@gmail.com>
2016-08-11 19:49:58 -0700

    Add feature test for adding project todo items

--- Gemfile
+++ Gemfile
@@ -47,4 +47,5 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'capybara'
+  gem 'launchy', '~> 2.4', '>= 2.4.3'
 end
--- spec/features/user_creates_a_new_project_item_spec.rb
+++ spec/features/user_creates_a_new_project_item_spec.rb
@@ -7,7 +7,10 @@ feature 'User creates a new project item' do
     visit project_path(1)
 
     click_link 'New Item'
+    fill_in 'item_action', with: "Call Steve Buscemi's agent"
+    find(".new_item input[type='submit']").click
 
-    expect(page).to have_content 'New Item'
+    expect(page).to have_content 'The Big Lebowski 2 Items'
+    expect(page).to have_content "Call Steve Buscemi's agent"
   end
 end
